76cbd65a59cd329312a97e4045d94c9c3720fc1f
Jiayi Hu <jiayi.ghu@gmail.com>
2017-10-10 21:07:01 +0200

    feat(haskell): add homework 1

---
+++ haskell/upenn/1-credit-cards.hs
@@ -0,0 +1,22 @@
+reversedToDigits :: Int -> [Int]
+reversedToDigits x
+  | x <= 0 = []
+  | x > 0 = (x `mod` 10) : reversedToDigits (x `div` 10)
+
+toDigits :: Int -> [Int]
+toDigits x = reverse (reversedToDigits x)
+
+doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther [x, y] = [x * 2, y]
+doubleEveryOther (x:y:xs)
+  | length xs `mod` 2 == 0 = [x * 2, y] ++ doubleEveryOther xs
+  | otherwise = [x, y * 2] ++ doubleEveryOther xs
+
+sumDigits :: [Int] -> Int
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+
+validate :: Int -> Bool
+validate xs = sumDigits(doubleEveryOther (toDigits xs)) `mod` 10 == 0
---
+++ haskell/upenn/README.md
@@ -0,0 +1,1 @@
+Exercises from the course [Introduction to Haskell](http://www.seas.upenn.edu/~cis194/spring13/lectures.html)
