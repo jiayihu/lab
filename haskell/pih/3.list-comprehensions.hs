95ae1cf9c468b3c5054eef67d8d1ea86cd0b8a41
Jiayi Hu <jiayi.ghu@gmail.com>
2017-10-14 22:18:02 +0200

    feat(pih): add list comprehensions exercises

--- haskell/pih/3.list-comprehensions.hs
+++ haskell/pih/3.list-comprehensions.hs
@@ -49,3 +49,26 @@ crack xs = encode (-factor) xs
     factor = head (positions (minimum chitab) chitab)
     chitab = [chisqr (rotate n table') table | n <- [0..25]]
     table' = freqs xs
+
+-- Exercises
+
+grid :: Int -> Int -> [(Int, Int)]
+grid m n = [(x, y) | x <- [0..m], y <- [0..n]]
+
+square :: Int -> [(Int, Int)]
+square n = [(x, y) | (x, y) <- grid n n, x /= y]
+
+replicate' :: Int -> a -> [a]
+replicate' n x = [x | _ <- [0..n - 1]]
+
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+factors :: Int -> [Int]
+factors n = [x| x <- [1..n], n `mod` x == 0]
+
+dropLast :: [a] -> [a]
+dropLast xs = take (length xs - 1) xs
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], sum (dropLast (factors x)) == x]
