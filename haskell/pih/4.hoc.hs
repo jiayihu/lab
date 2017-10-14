5d42c161172e16727173df51d785befe7bcaf79c
Jiayi Hu <jiayi.ghu@gmail.com>
2017-10-14 22:18:35 +0200

    feat(pih): add hoc chapter

---
+++ haskell/pih/4.hoc.hs
@@ -0,0 +1,72 @@
+import Data.List
+
+{-
+  Voting algorithms
+-}
+
+-- First past the post
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/=x) (rmdups xs)
+
+result :: Ord a => [a] -> [(Int, a)]
+result vs = sort [(count v vs, v)| v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+-- Alternative vote
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = (map snd) . result . (map head)
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+  [c] -> c
+  (c:_) -> winner' (elim c bs)
+
+
+{-
+  Exercises
+-}
+
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = and . map p
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = or . map p
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (x:xs)
+  | p x = x : takeWhile' p xs
+  | otherwise = []
+
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs)
+  | p x = dropWhile' p xs
+  | otherwise = x:xs
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f = foldr (\x xs -> if f x then x : xs else xs) []
+
+dec2int :: [Int] -> Int
+dec2int = foldl (\xs x -> xs * 10 + x) 0
