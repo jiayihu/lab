d1626108986b8ea6374d0f00db099302e36c2400
Jiayi Hu <jiayi.ghu@gmail.com>
2019-05-05 18:25:40 +0200

    feat(R): add chapter 9 exercises

---
+++ R/2-10.R
@@ -0,0 +1,78 @@
+# 10.
+# (a)
+library(MASS)
+?Boston
+dim(Boston)
+# 506 rows, 14 columns
+# 14 features, 506 housing values in Boston suburbs
+
+
+# (b)
+pairs(Boston)
+# X correlates with: a, b, c
+# crim: age, dis, rad, tax, ptratio
+# zn: indus, nox, age, lstat
+# indus: age, dis
+# nox: age, dis
+# dis: lstat
+# lstat: medv
+
+# (c)
+plot(Boston$age, Boston$crim)
+# Older homes, more crime
+plot(Boston$dis, Boston$crim)
+# Closer to work-area, more crime
+plot(Boston$rad, Boston$crim)
+# Higher index of accessibility to radial highways, more crime
+plot(Boston$tax, Boston$crim)
+# Higher tax rate, more crime
+plot(Boston$ptratio, Boston$crim)
+# Higher pupil:teacher ratio, more crime
+
+# (d)
+par(mfrow=c(1,3))
+hist(Boston$crim[Boston$crim>1], breaks=25)
+# most cities have low crime rates, but there is a long tail: 18 suburbs appear
+# to have a crime rate > 20, reaching to above 80
+hist(Boston$tax, breaks=25)
+# there is a large divide between suburbs with low tax rates and a peak at 660-680
+hist(Boston$ptratio, breaks=25)
+# a skew towards high ratios, but no particularly high ratios
+par(mfrow=c(1, 1))
+
+# (e)
+dim(subset(Boston, chas == 1))
+# 35 suburbs
+
+# (f)
+median(Boston$ptratio)
+# 19.05
+
+# (g)
+t(subset(Boston, medv == min(Boston$medv)))
+#              399      406
+# crim     38.3518  67.9208 above 3rd quartile
+# zn        0.0000   0.0000 at min
+# indus    18.1000  18.1000 at 3rd quartile
+# chas      0.0000   0.0000 not bounded by river
+# nox       0.6930   0.6930 above 3rd quartile
+# rm        5.4530   5.6830 below 1st quartile
+# age     100.0000 100.0000 at max
+# dis       1.4896   1.4254 below 1st quartile
+# rad      24.0000  24.0000 at max
+# tax     666.0000 666.0000 at 3rd quartile
+# ptratio  20.2000  20.2000 at 3rd quartile
+# black   396.9000 384.9700 at max; above 1st quartile
+# lstat    30.5900  22.9800 above 3rd quartile
+# medv      5.0000   5.0000 at min
+summary(Boston)
+# Not the best place to live, but certainly not the worst.
+
+# (h)
+dim(subset(Boston, rm > 7))
+# 64
+dim(subset(Boston, rm > 8))
+# 13
+summary(subset(Boston, rm > 8))
+summary(Boston)
+# relatively lower crime (comparing range), lower lstat (comparing range)
