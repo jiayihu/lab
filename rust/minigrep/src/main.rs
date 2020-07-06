2973f47b74a4264d7d2daa50b5ba0f49d6c9f7f2
Jiayi Hu <jiayi.ghu@gmail.com>
2020-07-06 21:47:40 +0200

    Add rust examples

---
+++ rust/minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
---
+++ rust/web_server/src/bin/main.rs
@@ -0,0 +1,47 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+use web_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (filename, status_line) = if buffer.starts_with(get) {
+        ("hello.html", "HTTP/1.1 200 OK\r\n\r\n")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("hello.html", "HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+        ("404.html", "HTTP/1.1 404 NOT FOUND\r\n\r\n")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
