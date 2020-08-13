f2113a1f0d32874286ab81fdb9e9616100a29db8
Jiayi Hu <jiayi.ghu@gmail.com>
2020-08-13 23:39:56 +0200

    feat(rust): add NTP clock synchronization

--- rust/clock/src/main.rs
+++ rust/clock/src/main.rs
@@ -1,5 +1,189 @@
-use chrono::{DateTime, Local, TimeZone};
+use byteorder::{BigEndian, ReadBytesExt};
+use chrono::{DateTime, Duration as ChronoDuration, Local, TimeZone, Utc};
 use clap::{App, Arg};
+use std::mem::zeroed;
+use std::net::UdpSocket;
+use std::time::Duration;
+
+const NTP_MESSAGE_LENGTH: usize = 48; // 12 * 4Bytes
+const NTP_TO_UNIX_SECONDS: i64 = 2_208_988_800;
+const LOCAL_ADDR: &'static str = "0.0.0.0:12300";
+
+#[derive(Default, Debug, Copy, Clone)]
+struct NTPTimestamp {
+    seconds: u32,
+    fraction: u32,
+}
+
+struct NTPMessage {
+    data: [u8; NTP_MESSAGE_LENGTH],
+}
+
+#[derive(Debug)]
+struct NTPResult {
+    t1: DateTime<Utc>,
+    t2: DateTime<Utc>,
+    t3: DateTime<Utc>,
+    t4: DateTime<Utc>,
+}
+
+impl NTPResult {
+    fn offset(&self) -> i64 {
+        let duration: chrono::Duration = (self.t4 - self.t1) - (self.t3 - self.t2);
+
+        duration.num_milliseconds()
+    }
+
+    fn delay(&self) -> i64 {
+        let duration: chrono::Duration = (self.t2 - self.t1) + (self.t4 - self.t3);
+
+        duration.num_milliseconds() / 2
+    }
+}
+
+impl From<NTPTimestamp> for DateTime<Utc> {
+    fn from(ntp: NTPTimestamp) -> Self {
+        let secs = ntp.seconds as i64 - NTP_TO_UNIX_SECONDS;
+        let mut nanos = ntp.fraction as f64;
+        nanos *= 1e9;
+        nanos /= 2_f64.powi(32);
+
+        Utc.timestamp(secs, nanos as u32)
+    }
+}
+
+impl From<DateTime<Utc>> for NTPTimestamp {
+    fn from(utc: DateTime<Utc>) -> Self {
+        let secs = utc.timestamp() + NTP_TO_UNIX_SECONDS;
+        let mut fraction = utc.timestamp_nanos() as f64;
+        fraction *= 2_f64.powi(32);
+        fraction /= 1e9;
+
+        NTPTimestamp {
+            seconds: secs as u32,
+            fraction: fraction as u32,
+        }
+    }
+}
+
+impl NTPMessage {
+    fn new() -> Self {
+        NTPMessage {
+            data: [0; NTP_MESSAGE_LENGTH],
+        }
+    }
+
+    fn client() -> Self {
+        const VERSION: u8 = 3; // 0000_0011
+        const MODE: u8 = 3;
+
+        let mut msg = NTPMessage::new();
+
+        msg.data[0] |= VERSION << 3;
+        msg.data[0] |= MODE;
+
+        msg
+    }
+
+    fn parse_timestamp(&self, i: usize) -> Result<NTPTimestamp, std::io::Error> {
+        let mut reader = &self.data[i..i + 8]; // 8bytes or 64bit
+        let seconds = reader.read_u32::<BigEndian>()?;
+        let fraction = reader.read_u32::<BigEndian>()?;
+
+        Ok(NTPTimestamp { seconds, fraction })
+    }
+
+    fn rx_time(&self) -> Result<NTPTimestamp, std::io::Error> {
+        self.parse_timestamp(32)
+    }
+
+    fn tx_time(&self) -> Result<NTPTimestamp, std::io::Error> {
+        self.parse_timestamp(40)
+    }
+}
+
+fn weighted_mean(values: &[f64], weights: &[f64]) -> f64 {
+    let mut sum = 0.0;
+    let mut sum_of_weights = 0.0;
+
+    for (value, weight) in values.iter().zip(weights) {
+        sum += value * weight;
+        sum_of_weights += weight;
+    }
+
+    sum / sum_of_weights
+}
+
+fn ntp_roundtrip(host: &str, port: u16) -> Result<NTPResult, std::io::Error> {
+    let destination = format!("{}:{}", host, port);
+    let timeout = Duration::from_secs(1);
+
+    let request = NTPMessage::client();
+    let mut response = NTPMessage::new();
+
+    let message = request.data;
+
+    let udp = UdpSocket::bind(LOCAL_ADDR)?;
+    udp.connect(&destination).expect("Unable to connect");
+
+    let t1 = Utc::now();
+    udp.send(&message)?;
+    udp.set_read_timeout(Some(timeout))?;
+    udp.recv_from(&mut response.data)?;
+    let t4 = Utc::now();
+
+    let t2: DateTime<Utc> = response.rx_time().unwrap().into();
+    let t3: DateTime<Utc> = response.tx_time().unwrap().into();
+
+    Ok(NTPResult { t1, t2, t3, t4 })
+}
+
+fn check_time() -> Result<f64, std::io::Error> {
+    const NTP_PORT: u16 = 123;
+
+    let servers = [
+        "time.nist.gov",
+        "time.apple.com",
+        "time.euro.apple.com",
+        "time.google.com",
+        "time2.google.com",
+        //"time.windows.com",
+    ];
+
+    let mut times = Vec::with_capacity(servers.len());
+
+    for &server in servers.iter() {
+        print!("{} => ", server);
+
+        let calc = ntp_roundtrip(&server, NTP_PORT);
+
+        match calc {
+            Ok(time) => {
+                println!(" {}ms away from local system time", time.offset());
+                times.push(time);
+            }
+            Err(_) => println!(" ? [response took too long]"),
+        };
+    }
+
+    let mut offsets = Vec::with_capacity(servers.len());
+    let mut offset_weights = Vec::with_capacity(servers.len());
+
+    for time in &times {
+        let offset = time.offset() as f64;
+        let delay = time.delay() as f64;
+
+        let weight = 1_000_000.0 / (delay * delay);
+        if weight.is_finite() {
+            offsets.push(offset);
+            offset_weights.push(weight);
+        }
+    }
+
+    let avg_offset = weighted_mean(&offsets, &offset_weights);
+
+    Ok(avg_offset)
+}
 
 struct Clock;
 
@@ -15,7 +199,7 @@ impl Clock {
 
         let t = t.with_timezone(&Local);
 
-        let mut systime: SYSTEMTIME = unsafe { std::mem::zeroed };
+        let mut systime: SYSTEMTIME = unsafe { zeroed };
 
         let dow = match t.weekday() {
             Weekday::Mon => 1,
@@ -60,7 +244,7 @@ impl Clock {
         use libc::{suseconds_t, time_t, timeval, timezone};
 
         let t = t.with_timezone(&Local);
-        let mut u: timeval = unsafe { std::mem::zeroed() };
+        let mut u: timeval = unsafe { zeroed() };
 
         u.tv_sec = t.timestamp() as time_t;
         u.tv_usec = t.timestamp_subsec_micros() as suseconds_t;
@@ -79,7 +263,7 @@ fn main() {
         .arg(
             Arg::with_name("action")
                 .takes_value(true)
-                .possible_values(&["get", "set"])
+                .possible_values(&["get", "set", "check-ntp"])
                 .default_value("get"),
         )
         .arg(
@@ -120,13 +304,20 @@ fn main() {
             None => println!("None"),
             _ => println!("Unable to set the time: {:?}", maybe_error),
         }
-    } else {
-        let now = Clock::get();
-        match std {
-            "timestamp" => println!("{}", now.timestamp()),
-            "rfc2822" => println!("{}", now.to_rfc2822()),
-            "rfc3339" => println!("{}", now.to_rfc3339()),
-            _ => unreachable!(),
-        }
+    } else if action == "check-ntp" {
+        let offset = check_time().unwrap() as isize;
+        let adjust_ms = offset.signum() * offset.abs().min(200) / 5;
+        let adjust_ms_ = ChronoDuration::milliseconds(adjust_ms as i64);
+        let now: DateTime<Utc> = Utc::now() + adjust_ms_;
+
+        println!("Now NTP: {}", now.to_rfc3339());
+    }
+
+    let now = Clock::get();
+    match std {
+        "timestamp" => println!("Now Clock: {}", now.timestamp()),
+        "rfc2822" => println!("Now Clock: {}", now.to_rfc2822()),
+        "rfc3339" => println!("Now Clock: {}", now.to_rfc3339()),
+        _ => unreachable!(),
     }
 }
