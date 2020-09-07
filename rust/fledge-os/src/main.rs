2c338e9d3ea2e7672dc6d1515bf44b3bc0e086da
Jiayi Hu <jiayi.ghu@gmail.com>
2020-09-07 15:25:18 +0200

    Refactor using VGA module

--- rust/fledge-os/src/main.rs
+++ rust/fledge-os/src/main.rs
@@ -2,85 +2,19 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 extern crate rlibc;
 
-use core::fmt;
 use core::fmt::Write;
 use core::panic::PanicInfo;
+use vga_buffer::ERR_WRITER;
 use x86_64::instructions::hlt;
-
-#[allow(unused)]
-#[derive(Copy, Clone)]
-#[repr(u8)]
-enum Color {
-    Black = 0x0,
-    Blue = 0x1,
-    Green = 0x2,
-    Cyan = 0x3,
-    Red = 0x4,
-    Magenta = 0x5,
-    Brown = 0x6,
-    Gray = 0x7,
-    DarkGray = 0x8,
-    BrightBlue = 0x9,
-    BrightGreen = 0xA,
-    BrightCyan = 0xB,
-    BrightRed = 0xC,
-    BrightMagenta = 0xD,
-    Yellow = 0xE,
-    White = 0xF,
-}
-
-struct Cursor {
-    position: isize,
-    foreground: Color,
-    background: Color,
-}
-
-impl Cursor {
-    fn color(&self) -> u8 {
-        let fg = self.foreground as u8;
-        let bg = (self.background as u8) << 4;
-
-        fg | bg
-    }
-
-    fn print(&mut self, text: &[u8]) {
-        let color = self.color();
-
-        let framebuffer = 0xb8000 as *mut u8;
-
-        for &character in text {
-            unsafe {
-                framebuffer.offset(self.position).write_volatile(character);
-                framebuffer.offset(self.position + 1).write_volatile(color);
-            }
-
-            self.position += 2;
-        }
-    }
-}
-
-impl fmt::Write for Cursor {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.print(s.as_bytes());
-        Ok(())
-    }
-}
 
 #[panic_handler]
 #[no_mangle]
 fn panic(info: &PanicInfo) -> ! {
-    let mut cursor = Cursor {
-        position: 0,
-        foreground: Color::White,
-        background: Color::Red,
-    };
-    for _ in 0..(80 * 25) {
-        cursor.print(b" ");
-    }
-    cursor.position = 0;
-    write!(cursor, "{}", info).unwrap();
+    write!(ERR_WRITER.lock(), "{}", info).unwrap();
 
     loop {
         hlt();
@@ -89,13 +23,7 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let text = b"Hello world";
-    let mut cursor = Cursor {
-        position: 0,
-        foreground: Color::Black,
-        background: Color::White,
-    };
-    cursor.print(text);
+    println!("Windows is shit");
 
     loop {
         hlt();
---
+++ rust/fledge-os/src/vga_buffer.rs
@@ -0,0 +1,169 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+#[allow(unused)]
+#[derive(Copy, Clone)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0x0,
+    Blue = 0x1,
+    Green = 0x2,
+    Cyan = 0x3,
+    Red = 0x4,
+    Magenta = 0x5,
+    Brown = 0x6,
+    Gray = 0x7,
+    DarkGray = 0x8,
+    BrightBlue = 0x9,
+    BrightGreen = 0xA,
+    BrightCyan = 0xB,
+    BrightRed = 0xC,
+    BrightMagenta = 0xD,
+    Yellow = 0xE,
+    White = 0xF,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(transparent)] // Represent struct with 8bit transparently as its field
+pub struct ColorCode(u8);
+
+impl ColorCode {
+    pub fn new(foreground: Color, background: Color) -> ColorCode {
+        let fg = foreground as u8;
+        let bg = (background as u8) << 4;
+
+        ColorCode(fg | bg)
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+pub struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    pub column_position: usize,
+    pub color_code: ColorCode,
+    pub buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // Printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Not printable ASCII
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    pub fn write_line(&mut self, s: &str) {
+        self.write_string(s);
+
+        let row = BUFFER_HEIGHT - 1;
+
+        for col in self.column_position..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(ScreenChar {
+                ascii_character: b' ',
+                color_code: self.color_code,
+            });
+        }
+
+        self.new_line();
+    }
+
+    pub fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::White, Color::Blue),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+    pub static ref ERR_WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::White, Color::Red),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
